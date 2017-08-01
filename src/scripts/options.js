e1ee814a827da4dc6d63fecee898a8f26a499446
Admin <tien@netpower.no>
2017-08-01 14:14:54 +0700

    remove algorithm input

--- src/scripts/options.js
+++ src/scripts/options.js
@@ -2,22 +2,18 @@ import ext from "./utils/ext";
 import storage from "./utils/storage";
 
 var wallet = document.getElementById('wallet');
-var algorithm = document.getElementById('algorithm');
 var button = document.getElementById('saveInformation');
 
 button.addEventListener('click', function() {
-  var data = { wallet: wallet.value, algorithm: algorithm.value };
+  var data = { wallet: wallet.value };
   storage.set({ data: data });
 }, false);
 
 storage.get('data', function(resp) {
-  var wallet_value = resp.data.wallet;
-  if(wallet_value) {
-    wallet.value = wallet_value;
-  }
-
-  var value_algorithm = resp.data.algorithm;
-  if(value_algorithm) {
-    algorithm.value = value_algorithm;
+  if(resp.data) {
+    var wallet_value = resp.data.wallet;
+    if (wallet_value) {
+      wallet.value = wallet_value;
+    }
   }
 });
